0934f9d0e880eb583ce7d18128568eefa5f40cfd
mynameisvinn <vin.tang@gmail.com>
2019-04-14 08:59:46 -0400

    clean up

--- Amarok.py
+++ Amarok.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Association_Mining(object):
+class Amarok(object):
     def __init__(self):
         pass
     
@@ -8,11 +8,17 @@ class Association_Mining(object):
         self.transactions_ = transactions
         
     def predict(self, term):
-        # fetch relevant associations
+        """
+        calculate lift, defined as p(y|x) / p(y), for each co-occuring item
+        """
+        
+        # identify items that co-occur with term
         unique_y = self._subset_y_given_X(term)
 
-        # calculate lift = p(y|x) / p(y)
+        # filter list - we're only interested in transactions containing term
         D_x = self._subset_D(term)
+
+        # finally, compute lift for each co-occuring item
         for y in unique_y:
             if y == term:
                 pass
@@ -21,10 +27,9 @@ class Association_Mining(object):
 
     def _subset_y_given_X(self, term):
         """
-        within all possible *transactions* containing 
-        *term*, find unique y's.
+        given *transactions* containing *term*, find unique y's.
         """
-        subsetted_transactions = self._subset_D(term)  # sublist of transaactions containing term
+        subsetted_transactions = self._subset_D(term)  # a filtered list of transaactions containing term
 
         # flatten into a single list and take the set
         flat_list = [item for sublist in subsetted_transactions for item in sublist]
@@ -32,14 +37,14 @@ class Association_Mining(object):
 
     def _subset_D(self, term):
         """
-        given a list of *transactions*, return a list
-        of transactions containing *term*.
+        given a list of transactions, return a filtered list of transactions containing term.
         """
         return list(filter(lambda transaction: term in transaction, self.transactions_))
 
     def _calc_prior(self, term, transactions):
         """
-        calculate p(term).
+        calculate p(term), which is the probability of seeing p(term) in the
+        entire sample.
         """
         n = np.sum([term in transaction for transaction in transactions])
         d = len(transactions)
